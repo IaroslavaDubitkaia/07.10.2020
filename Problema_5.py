d3afa5c671c9e1b03f61046098dff0b115f34fe5
IaroslavaDubitkaia <70658590+IaroslavaDubitkaia@users.noreply.github.com>
2020-10-06 21:00:51 +0300

    Add files via upload

---
+++ Problema_5.py
@@ -0,0 +1,2 @@
+n=int(input('Numarul lui Ion este'))
+print("Numerele pe care le spune Vasile sunt", n-2, n-1, n, n+1, n+2)
---
+++ Problema_6.py
@@ -0,0 +1,2 @@
+m=int(input('Mere primite:'))
+print("primul copil are", m-2, "mere, al doilea copil are", m+1, "mere")
