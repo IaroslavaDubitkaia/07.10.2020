dcc47a44a41440ec6f06f1532918fb80ec7d6362
IaroslavaDubitkaia <70658590+IaroslavaDubitkaia@users.noreply.github.com>
2020-10-06 21:40:20 +0300

    Add files via upload

---
+++ Problema_12.py
@@ -0,0 +1,5 @@
+a=int(input('Numarul de oua la prima gaina:'))
+b=int(input('Numarul de oua la a doua gaina:'))
+c=int(input('Numarul de oua la a treia gaina:'))
+d=int(input('Numarul de oua la a patra gaina:'))
+print("In o saptaman se obtin", 7*(a+b+c+d), "oua")
